390ac662c99bb339bf6859b74094617ab7fe7092
markedwardmurray <markedwardmurray@gmail.com>
2015-07-13 16:05:24 -0400

    fixed file structure

---
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  FISAppDelegate.m
+//  ApplesAndHolidays
+//
+//  Created by Al Tyus on 5/30/14.
+//  Copyright (c) 2014 com.flatironschool. All rights reserved.
+//
+
+#import "FISAppDelegate.h"
+
+@implementation FISAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSMutableDictionary *)launchOptions {
+
+    // Override point for customization after application launch.
+    
+    return YES;
+}
+
+
+
+@end
