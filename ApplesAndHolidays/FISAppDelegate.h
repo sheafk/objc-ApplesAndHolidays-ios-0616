5b835f651bc7235fd1b2c3aec91b5202ddcd4a39
markedwardmurray <markedwardmurray@gmail.com>
2015-09-21 11:18:58 -0400

    replace solution

--- ApplesAndHolidays/FISAppDelegate.h
+++ ApplesAndHolidays/FISAppDelegate.h
@@ -18,4 +18,36 @@
  
  */
 
+// Apple Picker
+
+- (NSArray *)pickApplesFromFruits:(NSArray *)fruits;
+
+// Holiday Supplies
+
+- (NSArray *)holidaysInSeason:(NSString *)season
+                   inDatabase:(NSDictionary *)database;
+
+- (NSArray *)suppliesInHoliday:(NSString *)holiday
+                      inSeason:(NSString *)season
+                    inDatabase:(NSDictionary *)database;
+
+- (BOOL)holiday:(NSString* )holiday
+     isInSeason:(NSString *)season
+     inDatabase:(NSDictionary *)database;
+
+- (BOOL)supply:(NSString *)supply
+   isInHoliday:(NSString *)holiday
+      inSeason:(NSString *)season
+    inDatabase:(NSDictionary *)database;
+
+- (NSDictionary *)addHoliday:(NSString *)holiday
+                    toSeason:(NSString *)season
+                  inDatabase:(NSDictionary *)database;
+
+- (NSDictionary *)addSupply:(NSString *)supply
+                  toHoliday:(NSString *)holiday
+                   inSeason:(NSString *)season
+                 inDatabase:(NSDictionary *)database;
+
+
 @end
