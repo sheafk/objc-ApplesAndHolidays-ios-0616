3aa8483f9acae2dca183f4a77244f277b1a6eb60
sheafk <sheafureyking@gmail.com>
2016-05-23 12:06:56 -0400

    Done.

--- ApplesAndHolidays/FISAppDelegate.h
+++ ApplesAndHolidays/FISAppDelegate.h
@@ -6,10 +6,31 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/**
- 
- * Declare your methods here.
- 
- */
+- (NSArray *)pickApplesFromFruits: (NSArray *)fruits;
+
+- (NSArray *)holidaysInSeason: (NSString *)season
+             inDatabase: (NSDictionary *)database;
+
+- (NSArray *)suppliesInHoliday: (NSString *)holiday
+                      inSeason: (NSString *)season
+                    inDatabase: (NSDictionary *)database;
+
+- (BOOL) holiday: (NSString *) holiday
+      isInSeason: (NSString *)season
+      inDatabase: (NSDictionary *)database;
+
+- (BOOL) supply: (NSString *)supply
+    isInHoliday: (NSString *)holiday
+       inSeason: (NSString *) season
+     inDatabase: (NSDictionary *)database;
+
+- (NSDictionary *)addHoliday: (NSString *)holiday
+                    toSeason: (NSString *)season
+                  inDatabase: (NSDictionary *)database;
+
+- (NSDictionary *)addSupply: (NSString *)supply
+                  toHoliday: (NSString *)holiday
+                   inSeason: (NSString *)season
+                 inDatabase: (NSDictionary *)database;
 
 @end
--- ApplesAndHolidays/FISAppDelegate.m
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -11,10 +11,77 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+- (NSArray *)pickApplesFromFruits:(NSArray *)fruits {
+    
+    NSPredicate *occurencesOfApple = [NSPredicate predicateWithFormat:@"self CONTAINS 'apple'"];
+    NSArray *listOfApples = [fruits filteredArrayUsingPredicate:occurencesOfApple];
+    
+    
+    return listOfApples;
+}
+
+- (NSArray *)holidaysInSeason:(NSString *)season
+                   inDatabase:(NSDictionary *)database {
+    
+    NSArray *newArray = [database[season]allKeys];
+    
+    return newArray;
+}
+
+- (NSArray *)suppliesInHoliday:(NSString *)holiday
+                      inSeason:(NSString *)season
+                    inDatabase:(NSDictionary *)database
+{
+    NSMutableArray *supplies = [[NSMutableArray alloc]init];
+    
+    [supplies addObjectsFromArray: database[season][holiday]];
+    
+    return supplies;
+}
+
+- (BOOL)holiday:(NSString *)holiday
+     isInSeason:(NSString *)season
+     inDatabase:(NSDictionary *)database
+{
+    
+BOOL value =[[database[season] allKeys] containsObject:holiday];
+
+    return value;
+}
+
+- (BOOL)supply:(NSString *)supply
+   isInHoliday:(NSString *)holiday
+      inSeason:(NSString *)season
+    inDatabase:(NSDictionary *)database
+{
+    
+    BOOL answer =[database[season][holiday] containsObject:supply];
+    
+    return answer;
+}
+
+- (NSDictionary *)addHoliday:(NSString *)holiday
+                    toSeason:(NSString *)season
+                  inDatabase:(NSDictionary *)database
+{
+    NSDictionary *copyOfDatabase = [database mutableCopy];
+    NSMutableArray *new = [[NSMutableArray alloc]init];
+    
+    [copyOfDatabase[season] setObject:new forKey:holiday];
+    
+    return copyOfDatabase;
+}
+
+- (NSDictionary *)addSupply:(NSString *)supply
+                  toHoliday:(NSString *)holiday
+                   inSeason:(NSString *)season
+                 inDatabase:(NSDictionary *)database
+{
+    NSDictionary *copyOfDatabase = [database mutableCopy];
+    
+    [copyOfDatabase[season][holiday]addObject:supply];
+    
+    return copyOfDatabase;
+}
 
 @end
